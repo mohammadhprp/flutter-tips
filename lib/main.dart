d7417f5b4ad5298c7685c14de7d50b9c8c8bf873
Mohammad <mohammadhprp@gmail.com>
2023-05-20 13:46:12 +0330

    feat: update app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'constants/app_router.dart';
 import 'screens/home_screen.dart';
@@ -14,10 +16,17 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Tips and Components',
-      theme: ThemeData(
+      theme: FlexThemeData.light(
         useMaterial3: true,
-        primarySwatch: Colors.blue,
+        scheme: FlexScheme.blueM3,
+        textTheme: GoogleFonts.nunitoSansTextTheme(),
       ),
+      darkTheme: FlexThemeData.dark(
+        useMaterial3: true,
+        scheme: FlexScheme.blueM3,
+        textTheme: GoogleFonts.nunitoSansTextTheme(),
+      ),
+      themeMode: ThemeMode.system,
       debugShowCheckedModeBanner: false,
       home: const HomeScreen(),
       routes: appRouter(),
